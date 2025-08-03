df13ba77a1f3a4b26c477497d419d6cd150562bb
settarovartur <settarovartur2@gamil.com>
2025-08-03 16:42:06 +0500

    init

--- handlers.py
+++ handlers.py
@@ -16,3 +16,45 @@ def help(update : Update, context : CallbackContext):
 
 def echo(update : Update, context : CallbackContext):
     update.message.reply_text(update.message.text) 
+
+def echo_photo(update : Update, context : CallbackContext):
+    update.message.reply_photo(update.message.photo[-1].file_id)
+
+
+def echo_video(update : Update, context : CallbackContext):
+    update.message.reply_video(update.message.video.file_id)
+
+
+def echo_audio(update : Update, context : CallbackContext):
+    update.message.reply_audio(update.message.audio.file_id)
+
+
+def echo_sticker(update : Update, context : CallbackContext):
+    update.message.reply_sticker(update.message.sticker.file_id)
+
+
+
+def echo_voice(update : Update, context : CallbackContext):
+    update.message.reply_voice(update.message.voice.file_id)
+
+
+def echo_doc(update : Update , context : CallbackContext):
+    update.message.reply_document(update.message.document.file_id)
+
+def echo_Contact(update: Update, context: CallbackContext):
+    contact = update.message.contact
+    update.message.reply_contact(
+        phone_number=contact.phone_number,
+        first_name=contact.first_name,
+        last_name=contact.last_name if contact.last_name else ""
+    )
+
+
+def echo_location(update: Update, context: CallbackContext):
+    location = update.message.location
+    update.message.reply_location(latitude=location.latitude, longitude=location.longitude)
+
+
+def echo_video_note(update : Update, context : CallbackContext):
+    pass
+    
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from telegram.ext import Updater, CallbackContext , CommandHandler, MessageHandler , Filters
 from config import TOKEN
-from handlers import start, help, echo
+from handlers import (start, help, echo, echo_audio,
+                      echo_photo, echo_sticker, echo_video,
+                      echo_voice, echo_Contact,echo_doc,
+                      echo_location
+)
 
 
 
@@ -15,6 +19,19 @@ def main() -> None:
 #Message handler
 
     dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    dispatcher.add_handler(MessageHandler(Filters.photo, echo_photo))
+    dispatcher.add_handler(MessageHandler(Filters.audio , echo_audio))
+    dispatcher.add_handler(MessageHandler(Filters.sticker, echo_sticker))
+    dispatcher.add_handler(MessageHandler(Filters.voice, echo_voice))
+    dispatcher.add_handler(MessageHandler(Filters.video, echo_video))
+    dispatcher.add_handler(MessageHandler(Filters.contact, echo_Contact))
+    dispatcher.add_handler(MessageHandler(Filters.document,echo_doc))
+    dispatcher.add_handler(MessageHandler(Filters.location, echo_location))
+    
+
+
+    print(" Multi-Echo bot ishga tushdi...")
+
 
     updater.start_polling()
     updater.idle()
